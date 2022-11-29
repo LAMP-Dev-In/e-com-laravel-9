1a62f3d2f25e2b3e6a0309bda35cbe2ad6e1ab41
LAMP-Dev-In <lamp.developer.in@gmail.com>
2022-11-29 13:05:24 +0530

    data access with object notation

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -4,17 +4,17 @@
 
     <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
       <div class="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        @foreach($products as $product)
+          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="{{$product->id - 1}}" aria-label="Slide {{$product->id}}" class="{{$product->id==1?'active':''}}" aria-current="{{$product->id==1?'true':''}}"></button>
+        @endforeach
       </div>
       <div class="carousel-inner">
         @foreach($products as $product)
-        <div class="carousel-item {{$product['id']==1?'active':''}}">
-          <img class="slider-img" src="{{$product['gallery']}}" class="d-block w-100" alt="{{$product['name']}}">
+        <div class="carousel-item {{$product->id==1?'active':''}}">
+          <img class="slider-img" src="{{$product->gallery}}" class="d-block w-100" alt="{{$product->name}}">
           <div class="carousel-caption d-none d-md-block slider-text">
-            <h5>{{$product['name']}}</h5>
-            <p>{{$product['description']}}</p>
+            <h5>{{$product->name}}</h5>
+            <p>{{$product->description}}</p>
           </div>
         </div>      
         @endforeach
@@ -33,9 +33,9 @@
       <h3>Trending Products</h3>
       @foreach($products as $product)
         <div class="tranding-item">
-          <img class="tranding-image" src="{{$product['gallery']}}" class="d-block w-100" alt="{{$product['name']}}">
+          <img class="tranding-image" src="{{$product->gallery}}" class="d-block w-100" alt="{{$product->name}}">
           <div class="">
-            <h5>{{$product['name']}}</h5>
+            <h5>{{$product->name}}</h5>
           </div>
         </div>      
         @endforeach

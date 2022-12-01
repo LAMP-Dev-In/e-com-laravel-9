50b2fe060fb470ea0221f4d3279595b21175fff8
LAMP-Dev-In <lamp.developer.in@gmail.com>
2022-12-01 15:31:15 +0530

    product store api

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -21,4 +21,41 @@ class ProductController extends Controller
     {
         return Product::find($id);
     }
+
+    function store(Request $request)
+    {
+        try {
+
+            //code...
+            $product = new Product();
+            $product->name = $request->name;
+            $product->price = $request->price;
+            $product->category = $request->category;
+            $product->description = $request->description;
+            $product->gallery = $request->gallery;
+            $product->created_at = now();
+            $product->updated_at = now();
+            $product->save();
+
+            return response()->json([
+                'message' => 'product created successfully',
+                'data' => $product,
+                'status' => 200
+
+            ]);
+
+        } catch (\Throwable $th) {
+
+            //throw $th;
+            return response()->json([
+                'message' => 'product not created',
+                'data' => $product,
+                'status' => 201,
+                'err' => $th
+            ]);
+        }
+
+
+
+    }
 }
